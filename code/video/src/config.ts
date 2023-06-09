fd94143fc8edd29d7c97b2f046af58da513c0465
Luis Fernando <luisfernandinho094@gmail.com>
2023-06-09 10:38:13 -0300

    refactor: remove api and web folders

--- code/video/src/config.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  redis: {
-    port: process.env.REDIS_PORT && Number(process.env.REDIS_PORT) || 6379,
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    password: process.env.REDIS_PASSWORD || "",
-  },
-};
--- code/video/src/services/bull.ts
+++
@@ -1,6 +0,0 @@
-import Queue from "bull";
-import { config } from "../config.js";
-
-export const videoQueue = new Queue("video-processing", {
-  redis: config.redis,
-});
